f573aee28b43576926d9e27ba074d381ca055297
Jean Carlo Rivera Ura <jura@hera.local>
2025-06-09 18:27:57 +0200

    init db

--- infra/app_init.sh
+++ infra/app_init.sh
@@ -26,7 +26,7 @@ mkdir -p "$TARGET_DIR"
 echo "Changing directory to ${TARGET_DIR}..."
 cd "$TARGET_DIR"
 echo "Attempting to clone repository into ${TARGET_DIR}..."
-
+rm -rf ${TARGET_DIR}/revolut_interview
 git clone "${CLONE_URL}"
 
 if [ $? -ne 0 ]; then
@@ -35,22 +35,32 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Getting public IP for local instance..."
-PUBLIC_IP=`aws ec2 describe-instances \
-    --instance-ids ${INSTANCE_ID} \
-    --query "Reservations[].Instances[].PublicIpAddress" \
-    --output text`
 
-PRIVATE_IP=`aws ec2 describe-instances \
-    --instance-ids ${INSTANCE_ID} \
+
+PRIVATE_IP_HAPROXY=`aws ec2 describe-instances \
     --query "Reservations[].Instances[].PrivateIpAddress" \
+    --filters "Name=tag:application,Values=haproxy" "Name=instance-state-name,Values=running" \
     --output text`
 
-echo "run the next commands "
-
-echo "cd ${TARGET_DIR}/revolut_interview/app"
+if [ -z "$PRIVATE_IP_HAPROXY" ]; then
+    echo "No haproxy instance found. Please check the tag and instance state."
+    exit 1
+fi
+echo "changing directory to ${TARGET_DIR}/revolut_interview/app"
+cd ${TARGET_DIR}/revolut_interview/app
 
 echo "building docker image..."
 docker build -t app .
+if [ $? -ne 0 ]; then
+    echo "Failed to build Docker image. Please check the Dockerfile and build context."
+    exit 1
+fi
+echo "Docker image built successfully."
 echo "Create the user and tables"
-
-echo "docker run -e TARGET_IP_ADDRESS="192.168.1.100" -t -i -p 5000:5000 app"
+PGPASSWORD=postgres_password psql -h ${PRIVATE_IP_HAPROXY} -U postgres -f /opt/app/revolut_interview/app/init_db.sql
+if [ $? -ne 0 ]; then
+    echo "Failed to execute init.sql. Please check the SQL script and database connection."
+    exit 1
+fi
+echo "Running the application in a Docker container..."
+docker run -e TARGET_IP_ADDRESS=${PRIVATE_IP_HAPROXY} -t -i -p 5000:5000 app &
--- infra/main.tf
+++ infra/main.tf
@@ -114,7 +114,8 @@ resource "aws_instance" "haproxy_standalone" {
     availability_zone = "${var.region1}a"
     subnet_id = aws_subnet.private_subnet_region1[0].id
     vpc_security_group_ids = [aws_security_group.haproxy_sg_region1.id, 
-    aws_security_group.ssm_sg_region1.id]
+    aws_security_group.ssm_sg_region1.id,
+    aws_security_group.postgres_sg_region1.id]
     iam_instance_profile = aws_iam_instance_profile.session_manager_profile.name
     key_name = aws_key_pair.ssh_key_pair1.key_name
 
